184b0b9990006edf8de18c2c9aa481d512dc92bc
arisp99 <aris.paschalidis@gmail.com>
2020-03-31 16:28:27 -0400

    Changed description, added namespace, added lisense, added project documentation

---
+++ R/CAvaccines.R
@@ -0,0 +1,10 @@
+#' CAvaccines: A package for analyzing the relationship between vaccination
+#' rates and infectious disease occurence in california schools
+#'
+#' Project was undertaken as the final project for BIOL 1600.
+#'
+#' @docType package
+#' @name CAvaccines
+#'
+#'
+NULL
