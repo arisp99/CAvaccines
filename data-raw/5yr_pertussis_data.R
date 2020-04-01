2bfc142497d0ec83b61b13ffa3c5980181eb63b9
arisp99 <aris.paschalidis@gmail.com>
2020-04-01 01:36:49 -0400

    Changed names of variable to remove whitespace. In addition, made it clear what package functions where coming from. Specifically, added readxl:: in several places.

--- data-raw/5yr_pertussis_data.R
+++ data-raw/5yr_pertussis_data.R
@@ -12,11 +12,14 @@ yr_2013_2017 <- system.file(
 )
 
 # Extract relevant data from each file
-yr_2008_2012 <- read_excel(yr_2008_2012, sheet = "5Yr Pertussis", range = cell_rows(2:61))
-yr_2013_2017 <- read_excel(yr_2013_2017, sheet = "5Yr Pertussis", range = cell_rows(2:64))
+yr_2008_2012 <- readxl::read_excel(yr_2008_2012, sheet = "5Yr Pertussis", range = readxl::cell_rows(2:61))
+yr_2013_2017 <- readxl::read_excel(yr_2013_2017, sheet = "5Yr Pertussis", range = readxl::cell_rows(2:64))
 
 # Combine the files together to get data from 2008-2017
-pertussis    <- merge(yr_2008_2012, yr_2013_2017, by = "Jurisdiction")
+pertussis <- merge(yr_2008_2012, yr_2013_2017, by = "Jurisdiction")
+
+# Alter names so that there is no whitespace
+names(pertussis) <- gsub("[^[:alnum:]]", "", names(pertussis))
 
 # Change name of CALIFORNIA cell to TOTALS to be clearer
 pertussis[6, 1] = "TOTALS"
