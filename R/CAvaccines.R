e9c85f350f5f3c78cd157780627d2974423fa42e
arisp99 <aris.paschalidis@gmail.com>
2020-04-01 01:59:47 -0400

    Updated documentation for datasets.

--- R/CAvaccines.R
+++ R/CAvaccines.R
@@ -19,26 +19,26 @@ NULL
 #' @format A data frame with variables:
 #'   \describe{
 #'     \item{Jurisdiction}{Jurisdiction of California}
-#'     \item{2008 Cases}{Number of pertussis cases in 2008}
-#'     \item{2008 Rates}{Incidence rate per 100,000 in 2008}
-#'     \item{2009 Cases}{Number of pertussis cases in 2009}
-#'     \item{2009 Rates}{Incidence rate per 100,000 in 2009}
-#'     \item{2010 Cases}{Number of pertussis cases in 2010}
-#'     \item{2010 Rates}{Incidence rate per 100,000 in 2010}
-#'     \item{2011 Cases}{Number of pertussis cases in 2011}
-#'     \item{2011 Rates}{Incidence rate per 100,000 in 2011}
-#'     \item{2012 Cases}{Number of pertussis cases in 2012}
-#'     \item{2012 Rates}{Incidence rate per 100,000 in 2012}
-#'     \item{2013 Cases}{Number of pertussis cases in 2013}
-#'     \item{2013 Rates}{Incidence rate per 100,000 in 2013}
-#'     \item{2014 Cases}{Number of pertussis cases in 2014}
-#'     \item{2014 Rates}{Incidence rate per 100,000 in 2014}
-#'     \item{2015 Cases}{Number of pertussis cases in 2015}
-#'     \item{2015 Rates}{Incidence rate per 100,000 in 2015}
-#'     \item{2016 Cases}{Number of pertussis cases in 2016}
-#'     \item{2016 Rates}{Incidence rate per 100,000 in 2016}
-#'     \item{2017 Cases}{Number of pertussis cases in 2017}
-#'     \item{2017 Rates}{Incidence rate per 100,000 in 2017}
+#'     \item{2008Cases}{Number of pertussis cases in 2008}
+#'     \item{2008Rates}{Incidence rate per 100,000 in 2008}
+#'     \item{2009Cases}{Number of pertussis cases in 2009}
+#'     \item{2009Rates}{Incidence rate per 100,000 in 2009}
+#'     \item{2010Cases}{Number of pertussis cases in 2010}
+#'     \item{2010Rates}{Incidence rate per 100,000 in 2010}
+#'     \item{2011Cases}{Number of pertussis cases in 2011}
+#'     \item{2011Rates}{Incidence rate per 100,000 in 2011}
+#'     \item{2012Cases}{Number of pertussis cases in 2012}
+#'     \item{2012Rates}{Incidence rate per 100,000 in 2012}
+#'     \item{2013Cases}{Number of pertussis cases in 2013}
+#'     \item{2013Rates}{Incidence rate per 100,000 in 2013}
+#'     \item{2014Cases}{Number of pertussis cases in 2014}
+#'     \item{2014Rates}{Incidence rate per 100,000 in 2014}
+#'     \item{2015Cases}{Number of pertussis cases in 2015}
+#'     \item{2015Rates}{Incidence rate per 100,000 in 2015}
+#'     \item{2016Cases}{Number of pertussis cases in 2016}
+#'     \item{2016Rates}{Incidence rate per 100,000 in 2016}
+#'     \item{2017Cases}{Number of pertussis cases in 2017}
+#'     \item{2017Rates}{Incidence rate per 100,000 in 2017}
 #'   }
 #'
 #' @docType data
@@ -56,12 +56,12 @@ NULL
 #'
 #' @format A data frame with variables:
 #'   \describe{
-#'     \item{School Code}{School code specific to each school.}
+#'     \item{School_Code}{School code specific to each school.}
 #'     \item{Jurisdiction}{Jurisdiction of California.}
-#'     \item{School Type}{Indicates whether school is a public or private school.}
-#'     \item{School Name}{Name of the school.}
+#'     \item{School_Type}{Indicates whether school is a public or private school.}
+#'     \item{School_Name}{Name of the school.}
 #'     \item{Enrollment}{Number of students enrolled.}
-#'     \item{\% Up to Date}{Percent of up to date students. Up to date students
+#'     \item{Up_to_Date}{Percent of up to date students. Up to date students
 #'     must have four doses of polio vaccine (three doses are acceptable if at
 #'     least one dose was received on or after the fourth birthday), five
 #'     DTaP/DTP/DT vaccine doses (four doses are acceptable if at least one dose
@@ -71,36 +71,36 @@ NULL
 #'     first birthday. In addition, student must have three doses of hepatitis
 #'     B vaccine and one dose of varicella vaccine or physician-documented
 #'     varicella disease.}
-#'     \item{\% Conditional}{Percent of students that do not meet all
+#'     \item{Conditional}{Percent of students that do not meet all
 #'     requirenments. This may because the student lacks but is
 #'     not yet due for a required dose, has a physician affidavit of Temporary
 #'     Medical Exemption for one or more doses, or is a transfer student who
 #'     has no record yet.}
-#'     \item{\% PME}{Percent of students with a Permanent Medical Exemption (PME).
+#'     \item{PME}{Percent of students with a Permanent Medical Exemption (PME).
 #'     Indicates that the physical condition of the student or medical
 #'     circumstances relating to the student are such that immunization
 #'     is permanently not indicated.}
-#'     \item{\% PBE}{Percent of students with a Personal Belief Exemption (PBE),
+#'     \item{PBE}{Percent of students with a Personal Belief Exemption (PBE),
 #'     whereby a parent requests exemption from the immunization requirements
 #'     for school entry because all or some immunizations are contrary to the
 #'     parent's beliefs.}
-#'     \item{\% Others}{Percent of students reported as attending independent
+#'     \item{Others}{Percent of students reported as attending independent
 #'     study who do not receive classroom-based instruction or home-based
 #'     private schools or receiving IEP services.}
-#'     \item{\% Overdue}{Percent of students overdue for one or more immunizations.}
-#'     \item{\% DTP}{Percent of students with full DTP vaccination course. Full
+#'     \item{Overdue}{Percent of students overdue for one or more immunizations.}
+#'     \item{DTP}{Percent of students with full DTP vaccination course. Full
 #'     course defined as 4 or more doses of any diphtheria and tetanus toxoids
 #'     and pertussis vaccines including diphtheria and tetanus toxoids, and any
 #'     acellular pertussis vaccine (DTaP/DTP/DT).}
-#'     \item{\% Polio}{Percent of students with full Polio vaccination course.
+#'     \item{Polio}{Percent of students with full Polio vaccination course.
 #'     Full course defined as 3 or more doses of polio vaccine; oral polio
 #'     vaccine (OPV) or inactivated polio vaccine (IPV) or any combination
 #'     of these.}
-#'     \item{\% MMR}{Percent of students with full MMR vaccination course.
+#'     \item{MMR}{Percent of students with full MMR vaccination course.
 #'     Full course defined as 2 doses of measles-mumps-rubella vaccine.}
-#'     \item{\% HepB}{Percent of students with full HepB vaccination course.
+#'     \item{HepB}{Percent of students with full HepB vaccination course.
 #'     Full course defined as 3 or more doses of Hepatitis B vaccine.}
-#'     \item{\% Var}{Percent of students with full Varicella vaccination course.
+#'     \item{Var}{Percent of students with full Varicella vaccination course.
 #'     Full course defined as 1 or more doses of varicella at or after child's
 #'    first birthday, unadjusted for history of varicella illness.}
 #'     \item{Reported}{A binary variable indicating whether the school reported
