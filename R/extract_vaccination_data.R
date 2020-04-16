e5dbcce756af577ce4b8e8c822f7d82a106598c5
arisp99 <aris.paschalidis@gmail.com>
2020-04-15 22:47:25 -0400

    Added the ability for our function to work with excel files that differed from one another. Now, this function works for the years of 2008 till 2017 (all the years we have case date). The function also works for 2018, what we have been using in our analysis file so far.

--- R/extract_vaccination_data.R
+++ R/extract_vaccination_data.R
@@ -5,45 +5,70 @@
 #' @param file The file that contains the data to be extracted
 #' @param year The year that the data was collected
 #' @param rows The minimum and maximum rows to be extracted
+#' @param sheet The sheet that contains the data in the excel file
 #' @return Returns the county level vaccination dataframe
 #'
 #' @export
-extract_vaccination <- function(file, year, rows){
+extract_vaccination <- function(file, year, rows, sheet){
   yr <- system.file("extdata", file, package = "CAvaccines")
 
   # Extract relevant data from each file
   yr <- suppressMessages(
     readxl::read_excel(yr,
-                       sheet = "Enrollment 20 or More",
+                       sheet = sheet,
                        range = readxl::cell_rows(rows),
                        na = ".",
                        col_names = FALSE))
 
   # Select a subset of variables and remove all schools that did not report
-  yr <- yr %>%
-    stats::setNames(1:30) %>%
-    dplyr::select(.data$`1`:.data$`3`, .data$`6`:.data$`7`, .data$`9`,
-                  .data$`11`, .data$`13`, .data$`15`, .data$`17`,
-                  .data$`19`, .data$`21`, .data$`23`, .data$`25`,
-                  .data$`27`, .data$`29`, .data$`30`) %>%
-    dplyr::filter(.data$`30` != "N")
+  if (year %in% c("2008", "2009", "2010", "2011")){
+    # Here we do not need to filter
+    selection <- c("a", "b", "c", "e", "f", "h", "j", "l", "n", "p", "r", "v", "x", "z")
 
-  # Remove less than or equal, greater than or equal, and percent from data
-  yr[, 6:16] <- as.data.frame(gsub("[^[:alnum:]]", "", as.matrix(yr[, 6:16])))
-  for (i in 6:16){
-    yr[,i] = as.numeric(unlist(yr[,i]))
+    colnames(yr) <- make.unique(rep(letters, length.out = ncol(yr)), sep='')
+    yr <- dplyr::select(yr, selection)
+
+  } else{
+    # Here we need to filter
+    if (year == "2012"){
+      selection <- c("a", "b", "c", "f", "g", "i", "k", "m", "o", "q", "s", "w", "y", "a1", "b1")
+    } else if (year %in% c("2013", "2014")){
+      selection <- c("a", "b", "c", "f", "g", "i", "k", "m", "o", "q", "s", "u", "w", "y", "z")
+    } else if (year %in% c("2015", "2016")){
+      selection <- c("a", "b", "c", "f", "g", "i", "k", "m", "o", "w", "y", "a1", "c1", "e1", "f1")
+    } else if (year %in% c("2017", "2018")){
+      selection <- c("a", "b", "c", "f", "g", "i", "k", "m", "o", "u", "w", "y", "a1", "c1", "d1")
+    } else{print("Wrong year was inputed!")}
+
+    colnames(yr) <- make.unique(rep(letters, length.out = ncol(yr)), sep='')
+    yr <- dplyr::select(yr, selection)
+    yr <- dplyr::filter(yr, yr[ncol(yr)] != "N")
+    yr[ncol(yr)] <- NULL
+  }
+
+  # Remove less than or equal, greater than or equal, and percent from data.
+  # Note that we only do this for a subset of the data as the other datasets
+  # had problems here
+  if (year %in% c("2013", "2017", "2018")){
+    yr[, 5:14] <- as.data.frame(gsub("[^[:digit:]*\\.*[:digit:]*$]", "", as.matrix(yr[, 5:14])))
+    for (i in 5:14){
+      yr[, i] = as.numeric(unlist(yr[, i]))
+    }
   }
 
   # Label the columns of the dataset
   colnames(yr) <-
     c("School_Code", "Jurisdiction", "School_Type", "School_Name", "Enrollment",
-      "Up_to_Date", "Conditional", "PME", "PBE", "Others", "Overdue",
-      "DTP", "Polio", "MMR", "HepB", "Var", "Reported")
+      "Up_to_Date", "Conditional", "PME", "PBE",
+      "DTP", "Polio", "MMR", "HepB", "Var")
 
   # Change name of dataset and turn Jurisdiction into a factor, turn into lowercase
   school_yr <- as.data.frame(yr)
   school_yr$Jurisdiction <- tolower(school_yr$Jurisdiction)
   school_yr$Jurisdiction <- as.factor(school_yr$Jurisdiction)
+
+  # Make sure Enrollment is numeric
+  school_yr$Enrollment <- as.numeric(school_yr$Enrollment)
 
   # We now take the school level vaccination data and create county level
   # vaccination data. For each measurment, we find the total number of kids per
@@ -57,8 +82,6 @@ extract_vaccination <- function(file, year, rows){
       total_Conditional = sum(.data$Conditional/100*.data$Enrollment),
       total_PME         = sum(.data$PME/100*.data$Enrollment),
       total_PBE         = sum(.data$PBE/100*.data$Enrollment),
-      total_Others      = sum(.data$Others/100*.data$Enrollment),
-      total_Overdue     = sum(.data$Overdue/100*.data$Enrollment),
       total_DTP         = sum(.data$DTP/100*.data$Enrollment),
       total_Polio       = sum(.data$Polio/100*.data$Enrollment),
       total_MMR         = sum(.data$MMR/100*.data$Enrollment),
